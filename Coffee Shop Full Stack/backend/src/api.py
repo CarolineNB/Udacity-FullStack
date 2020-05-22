ba19a1989080b3716fc19130775111ee04af53f3
Caroline B <eniloracb4@gmail.com>
2020-05-21 19:07:27 -0600

    pep8 style guidelines:

--- Coffee Shop Full Stack/backend/src/api.py
+++ Coffee Shop Full Stack/backend/src/api.py
@@ -16,26 +16,26 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-#db_drop_and_create_all()
+db_drop_and_create_all()
 
-## ROUTES
 '''
 @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True, "drinks":
+        drinks} where drinks is the list of drinks or appropriate
+        status code indicating reason for failure
 '''
 @app.route('/drinks', methods=['GET'])
 def get_drinks():
     try:
         drinks = Drink.query.all()
-        if len(drinks)==0:
+        if len(drinks) == 0:
             abort(404)
 
         drinks = [drink.short() for drink in drinks]
-        
+
         return jsonify({
             'success': True,
             'drinks': drinks
@@ -49,7 +49,8 @@ def get_drinks():
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        returns status code 200 and json {"success": True,
+        "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks-detail', methods=['GET'])
@@ -71,14 +72,17 @@ def get_drink_details(jwt):
     except Exception as error:
         raise error
 
+
 '''
 @TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True,
+        "drinks": drink} where drink an array containing only
+        the newly created drink or appropriate status code
+        indicating reason for failure
 '''
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
@@ -101,6 +105,7 @@ def add_new_drink(payload):
     except Exception:
         abort(422)
 
+
 '''
 @TODO implement endpoint
     PATCH /drinks/<id>
@@ -109,8 +114,10 @@ def add_new_drink(payload):
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True,
+        "drinks": drink} where drink an array containing only
+        the updated drink or appropriate status code indicating
+        reason for failure
 '''
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
@@ -125,12 +132,11 @@ def edit_drink(payload, drink_id):
         if title == '':
             abort(400)
         drink.title = title
-        
+
         recipe_data = body.get('recipe')
         if type(recipe_data) != list:
             abort(400)
         drink.recipe = json.dumps(recipe_data)
-        
 
         drink.update()
 
@@ -149,7 +155,8 @@ def edit_drink(payload, drink_id):
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+        returns status code 200 and json {"success": True,
+        "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
@@ -159,7 +166,7 @@ def remove_drink(payload, drink_id):
         drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
         if not drink:
             abort(404)
-        
+
         drink.delete()
 
         return jsonify({
@@ -169,23 +176,25 @@ def remove_drink(payload, drink_id):
     except Exception:
         abort(422)
 
-## Error Handling
+
+# Error Handling
 '''
 Example error handling for unprocessable entity
 '''
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
+
 
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 404,
                     "message": "resource not found"
                     }), 404
@@ -207,9 +216,10 @@ def bad_request(error):
         "messange": "bad request"
     }), 401
 
+
 '''
 @TODO implement error handler for 404
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
 @app.errorhandler(404)
 def not_found(error):
@@ -231,7 +241,7 @@ def internal_server_error(error):
 
 '''
 @TODO implement error handler for AuthError
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
 @app.errorhandler(AuthError)
 def authentication_error(error):
